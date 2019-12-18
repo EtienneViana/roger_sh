352d5dcd84dc43622655c0fe5ed6b8f834e99b0c
EtienneViana <etienne.viana@gmail.com>
2019-12-18 16:03:12 +0100

    deploy

---
+++ deploy.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# This deployement script is to be executed as root
+
+# Install basic tools
+apt-get -y -q install sudo
+apt-get -y -q install net-tools
+apt-get -y -q install vim
+
+# Create user and add rights
+adduser eviana
+usermod -aG eviana
+
+# Get configuration packages on private Github repository
+apt-get -y -q install git
+git clone https://EtienneViana@github.com/EtienneViana/roger_skyline_deploy.git /root/deployment_pack
+
+# Replace configuration files
+cd /root/deployment_pack
+cp interfaces /etc/network/interfaces
+cp sshd_config /etc/ssh/sshd_config
+
+# Add public_key in eviana's authorized_keys for ssh connexion
+cp authorized_keys /home/eviana/.ssh/authorized_keys
+
+# Setting firewall
+apt-get install iptables-persistent
+service netfilter-persistent start
+
+iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
+iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL ALL -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP
+iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -j DROP
+iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP
+iptables -t mangle -A PREROUTING -s 192.0.2.0/24 -j DROP
+iptables -t mangle -A PREROUTING -s 192.168.0.0/16 -j DROP
+iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP
+iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP
+iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
+iptables -A INPUT -p tcp --syn --dport 80 -m connlimit --connlimit-above 30 -j DROP
+iptables -A INPUT -p tcp --syn --dport 443 -m connlimit --connlimit-above 30 -j DROP
+
+service netfilter-persistent save
+
+# Final reboot
+reboot
